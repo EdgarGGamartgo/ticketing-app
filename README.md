1c4e2dc6d3dd388e6d874964443569967d20fc13
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-01-06 14:45:37 -0600

    Signin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ticketing-app
 Microservices approach for a ticketing app
+
+# For creating a SECRET inside my kubernetes NODE so that all my # pods can get access to it through ENV_VAR
+
+kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,10 +8,15 @@ import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
+import cookieSession from 'cookie-session'
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
-
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
@@ -24,6 +29,11 @@ app.all('*', async(req, res) => {
 app.use(errorHandler)
 
 const start = async() => {
+
+    if (!process.env.JWT_KEY) {
+      throw new Error('JWT_KEY must be defined')
+    }
+
     try {
       await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
         useNewUrlParser: true,
