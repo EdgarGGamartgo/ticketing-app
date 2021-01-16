f84bad16d4d675551465952ea48f34f738d8a450
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-01-16 17:39:38 -0600

    Adding nats

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Microservices approach for a ticketing app
 # For creating a SECRET inside my kubernetes NODE so that all my # pods can get access to it through ENV_VAR
 
 kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
+
+# http://localhost:8222/streaming/channelsz?subs=1 ----> NATS-STREAMING-SERVER HEALTH CHECK
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,12 +1,14 @@
 import nats from 'node-nats-streaming'
 
+console.clear()
+
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 })
 
 stan.on('connect', () => {
     console.log('Publisher connected to NATS')  //  nats-depl-7c48fbb75d-7jl4b //   kubectl port-forward nats-depl-7c48fbb75d-7jl4b 4222:4222
-
+                                                // rs ----> Command to restart publisher/listener instances
     const data = JSON.stringify({
         id: '123',
         title: 'concert',
