3cb444fbd6e0aafba376a1a581aacdbffa160f20
EdgarGGamartgo <edgarggamartgo@gmail.com>
2021-01-19 23:41:03 -0600

    Adding working NATS

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -1,8 +1,7 @@
 import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
-
-jest.mock('../../nats-wrapper')
+import { natsWrapper } from '../../nats-wrapper'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
    const response = await request(app).post('/api/tickets').send({})
@@ -83,4 +82,16 @@ it('creates a ticket with valid inputs', async () => {
     expect(tickets[0].title).toEqual(title)
 })
 
+it('publishes an event', async () => {
+    const title = 'fsdfsd'
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price: 20
+        })
+        .expect(201)
 
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
